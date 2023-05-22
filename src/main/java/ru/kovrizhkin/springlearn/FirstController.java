6cbff235aed6dcfbf004ffa3cba541f6489a5ce9
kovrizhka <pivsru@gmail.com>
2023-05-22 22:33:47 +0500

    created first spring-mvc project

---
+++ src/main/java/ru/kovrizhkin/springlearn/FirstController.java
@@ -0,0 +1,13 @@
+package ru.kovrizhkin.springlearn;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
